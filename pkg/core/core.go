b96372ac0e2c8c0972256ed1f388e3398873fe7c
Mathijs van Mourick <14082160+mmourick@users.noreply.github.com>
2024-01-09 23:42:20 +0100

    feat: add dns server port flag

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -50,6 +50,7 @@ func PrepareDNSQuery(domainName string, queryType uint16) dns.Msg {
 
 // SendDNSQuery sends a DNS query to a given DNS server.
 func SendDNSQuery(client *dns.Client, msg dns.Msg, dnsServerIP string) (*dns.Msg, time.Duration, error) {
+func SendDNSQuery(client *dns.Client, msg dns.Msg, dnsServerIP, dnsServerPort string) (*dns.Msg, time.Duration, error) {
 	if dnsServerIP == "" {
 		goOS := runtime.GOOS
 		if goOS == windows {
